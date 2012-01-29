a2914a2d4ea431f8b64316a5bdd19dc1b9e56e6e
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-01-28 21:32:51 -0500

    Re-added iOS stuff

---
+++ ios/CardsAgainstHumanity/CardsAgainstHumanity/Room.h
@@ -0,0 +1,13 @@
+//
+//  Room.h
+//  CardsAgainstHumanity
+//
+//  Created by Vivek Bhagwat on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Room : NSObject
+
+@end
---
+++ ios/CardsAgainstHumanity/CardsAgainstHumanity/Room.m
@@ -0,0 +1,13 @@
+//
+//  Room.m
+//  CardsAgainstHumanity
+//
+//  Created by Vivek Bhagwat on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Room.h"
+
+@implementation Room
+
+@end
