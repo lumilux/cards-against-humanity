bf6b852483974000bb69e12e93bea7c771d1c03d
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-01-28 19:16:30 -0500

    Initial Commit

---
+++ CardsAgainstHumanity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardsAgainstHumanity
+//
+//  Created by Vivek Bhagwat on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
